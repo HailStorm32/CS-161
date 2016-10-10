10ec3e8736f290d7de5bf7a0158f3f8180a7eb9b
Demetri V.S <demetriv.s.7@gmail.com>
2016-10-10 09:03:58 -0700

    First commit

---
+++ Geometry_Fun/Geometry_Fun/GeometryFun.cpp
@@ -0,0 +1,42 @@
+/*
+Program Name: Geometry_Fun
+Author:	Demetri Van Sickle
+Date: 10/10/16
+IDE: Visual Studio 2015 Enterprise 
+Description: Using a positive value it was given, the code will use that value to spit out the area and perimeter of a square, and radius, perimeter, and area of a circle. 
+*/
+
+#include <iostream>
+using namespace std;
+
+const float PI = 3.14159;
+
+void main()
+{
+	bool flag = true;
+	
+	int UserInput = 0;
+
+	int SquareLength = 0;
+	int SquareArea = 0;
+	int SquarePrim = 0;
+
+	int CircleDiameter = 0;
+	int CirclePrim = 0;
+	int CircleArea = 0;
+
+	while (flag == true)
+	{
+
+		cout << "Please enter a positive whole number:" << endl;
+
+		cin >> UserInput;
+
+		if (UserInput < 0)
+		{
+			count
+		}
+
+	}
+
+}
