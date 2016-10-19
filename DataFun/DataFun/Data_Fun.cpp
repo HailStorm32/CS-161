685adcf3ff6caf4008be90ec14bf96521819dd02
Demetri V.S <demetriv.s.7@gmail.com>
2016-10-19 16:38:21 -0700

    Added new assignment DataFun

---
+++ DataFun/DataFun/Data_Fun.cpp
@@ -0,0 +1,16 @@
+/*
+Program Name:  Data Fun
+Author:  Demetri Van Sickle
+Date:  10/19/16
+IDE:  Visual Studio 2015 Enterprise
+Description:  Take a persons favorite number (integer, and floating point) and letter. It will then display fun facts regaring those inputs
+*/
+/*
+				////////////////---pseudocode---///////////////////////
+
+							//////--Part_1--//////
+
+
+
+
+*/
