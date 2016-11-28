28df77d2c14ce4746194aa3ad539753e1152c941
Demetri V.S <demetriv.s.7@gmail.com>
2016-11-28 15:16:02 -0800

    Added new assignment String Demo

---
+++ String_Demo/String_Demo/StringDemo.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstring>
+#include <string>
+
+using namespace std;
+
+//Main Prototypes
+void cStringDemo();
+void stringTypeDemo();
+
+//CString prototypes
+void getName()
+
+//String prototypes
